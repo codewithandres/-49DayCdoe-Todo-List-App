7e9bb8fe86b74d63ae39ddcd64c3008adb63f570
Dize dev <axiozweb2022@gmail.com>
2024-05-18 22:28:55 -0500

    guardamos los todo en el localStororage

---
+++ js/app.js
@@ -0,0 +1,24 @@
+
+const taskInput = document.querySelector('.task-input input');
+
+let todos = JSON.parse(localStorage.getItem('todo-list'));
+
+taskInput.addEventListener('keyup', e => {
+    let userTask = taskInput.value.trim();
+
+    if (e.key === 'Enter' && userTask) {
+
+        if (!todos) { todos = [] };
+
+        taskInput.value = '';
+
+        let taskInfo = {
+            name: userTask,
+            status: 'pending'
+        };
+        todos.push(taskInfo);
+        localStorage.setItem('todo-list', JSON.stringify(todos));
+        //showTodos();
+    };
+
+});
